0cf8a3ecc0bfef60285d6ad44aa761aae7acad13
Gabriel Gigante <gabrielgigante29@gmail.com>
2024-04-23 13:50:34 -0300

    fix: fixes untrack alias

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# Brew
-/home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-
-# Starship theme
-starship init fish | source

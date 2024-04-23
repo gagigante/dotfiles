ca4b133dd447c2621632e327ec87c706761b89c8
Gabriel Gigante <gabrielgigante29@gmail.com>
2024-04-23 14:27:48 -0300

    chore: moving log functions to another file

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,24 +1,8 @@
 #! /usr/bin/bash
 
+source ./logger.sh
+
 DOTFILES=$(pwd -P)
-
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
 
 link_file () {
     local src=$1 dst=$2
